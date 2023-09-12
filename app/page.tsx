73abe5e9ca806e8a5b203e1a4794ab69fda04220
shaurya5 <gargshaurya28@gmail.com>
2023-09-12 15:32:49 +0530

    Frontend changes and contact form added with resend integrated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
+import Introduction from "@/components/introduction";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
+
 export default function Home() {
   return (
-    <main className="">
-      
+    <main className="flex flex-col items-center px-4">
+      <Introduction />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
   )
 }
